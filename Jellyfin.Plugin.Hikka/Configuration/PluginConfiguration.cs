cbe285525babfd84914982dfd25c26a87dc96f3e
Hot Masya <i.am.masya1@gmail.com>
2025-01-04 23:13:50 +0200

    added config page

--- Jellyfin.Plugin.Hikka/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Hikka/Configuration/PluginConfiguration.cs
@@ -1,7 +1,9 @@
+using Jellyfin.Plugin.Hikka.Types.Enums;
 using MediaBrowser.Model.Plugins;
 
 namespace Jellyfin.Plugin.Hikka.Configuration;
 
 public class PluginConfiguration : BasePluginConfiguration
 {
+    public Language PreferredLanguage { get; set; }
 }
---
+++ Jellyfin.Plugin.Hikka/Types/Enums/Language.cs
@@ -0,0 +1,7 @@
+namespace Jellyfin.Plugin.Hikka.Types.Enums;
+
+public enum Language
+{
+    En,
+    Ua
+}
